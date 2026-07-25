3426bf9070bd739a944fd56012f16c6aab68920c
sid <161123700+sidwgit@users.noreply.github.com>
2026-07-25 18:23:44 +0000

    Add Floor Number [A] from Codeforces

---
+++ 1426/A - Floor Number/solution.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    int t;
+    cin >> t;
+ 
+    while (t--) {
+        int n, x;
+        cin >> n >> x;
+ 
+        if (n <= 2)
+            cout << 1 << "
+";
+        else
+            cout << (n - 3) / x + 2 << "
+";
+    }
+ 
+    return 0;
+}
