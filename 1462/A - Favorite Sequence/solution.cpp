09952e3d41d0967e6f397c2d809c378984a4c686
sid <161123700+sidwgit@users.noreply.github.com>
2026-07-25 18:38:17 +0000

    Add Favorite Sequence [A] from Codeforces

---
+++ 1462/A - Favorite Sequence/solution.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    int t;
+    cin >> t;
+ 
+    while (t--) {
+        int n;
+        cin >> n;
+ 
+        vector<int> a(n);
+        for (int i = 0; i < n; i++)
+            cin >> a[i];
+ 
+        int l = 0, r = n - 1;
+ 
+        while (l <= r) {
+            cout << a[l] << " ";
+            l++;
+ 
+            if (l <= r) {
+                cout << a[r] << " ";
+                r--;
+            }
+        }
+ 
+        cout << "
+";
+    }
+ 
+    return 0;
+}
