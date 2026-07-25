6418ac51745c0c385620b7f345dc388fd3b1dac0
starboylive <ramadalivenkatesh@gmail.com>
2026-07-25 16:40:37 +0530

    solved 1399A problem

---
+++ codeforces/1399A.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h> 
+using namespace std; 
+
+int main(){
+
+    int t; 
+    cin >> t; 
+
+    while(t--){
+        int n; 
+        cin >> n; 
+
+        vector<int> a(n); 
+        for(int i=0; i < n; i++){
+            cin >> a[i]; 
+        }
+
+        sort(a.begin(), a.end()); 
+
+        bool ok = true;; 
+        for(int i=1; i < n; i++){
+            if(a[i] - a[i - 1] > 1){
+                ok = false; 
+                break; 
+            }
+        }
+        
+        cout << (ok ? "Yes" : "No") << '\n'; 
+
+    }
+
+    return 0; 
+}
